154dda90ba44f6ada44ff1fd02afac03098d78fc
Luan Barradas <barradas.luk@gmail.com>
2023-11-17 12:04:18 -0300

    senhas postgres

--- src/conexao.js
+++ src/conexao.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    hots: 'localhost',
+    host: 'localhost',
     port: 5432,
     user: 'postgres',
-    password: '123456',
-    database: 'dindin'
+    database: 'dindin',
+    password: '123456' // password: '123456789'
 });
 
 module.exports = pool;
